020ca2e3b4619557851c1741fad3cfe03a5e0222
Kuba <jklasecki@gmail.com>
2020-02-20 11:53:40 +0100

    Updates

---
+++ precache-manifest.b6192f93adae3309345e018f7b378617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25dc3981f25e29437d8dbfff1c52d17e",
+    "url": "/when-holiday/index.html"
+  },
+  {
+    "revision": "7511da2dd0ed89cf412f",
+    "url": "/when-holiday/static/css/main.c91313ea.chunk.css"
+  },
+  {
+    "revision": "e3359fa1cc8a56a390ae",
+    "url": "/when-holiday/static/js/2.f9c02524.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/when-holiday/static/js/2.f9c02524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7511da2dd0ed89cf412f",
+    "url": "/when-holiday/static/js/main.5cf0be66.chunk.js"
+  },
+  {
+    "revision": "0bee922e23cde791ae21",
+    "url": "/when-holiday/static/js/runtime-main.8377960e.js"
+  }
+]);
